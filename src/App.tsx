9b193c0b70bbf0cf02f0c6f48c37dbd3d55baa49
fmn <cdnxyf@gmail.com>
2018-07-19 22:43:33 +0800

    tag添加closable属性 styles属性

--- src/App.tsx
+++ src/App.tsx
@@ -6,12 +6,22 @@ class App extends React.Component {
   public render() {
     return (
       <div className="App">
-        <Tag/>
-        <Tag type="success"/>
-        <Tag type="info"/>
-        <Tag type="warning"/>
-        <Tag type="danger"/>
-        <Tag type="other" />
+        <Tag>标签一</Tag>
+        <Tag type="success" />
+        <Tag type="info" />
+        <Tag type="warning" />
+        <Tag type="danger" />
+        <Tag style={{ backgroundColor: 'rgba(255, 64, 194, .1)', borderColor: 'rgba(255, 64, 194, .2)', color: 'rgb(255, 64, 194)' }} />
+        <br />
+        <Tag size="medium" />
+        <Tag size="small" type="info" />
+        <Tag size="mini" type="danger" />
+        <br />
+        <Tag closable={true}>标签一</Tag>
+        <Tag type="success" closable={true} />
+        <Tag type="info" closable={true} />
+        <Tag type="warning" closable={true} />
+        <Tag type="danger" closable={true} />
       </div>
     );
   }
--- src/components/tag/src/tag.tsx
+++ src/components/tag/src/tag.tsx
@@ -8,7 +8,9 @@ export interface ITagProps {
     /** 标签是否可以关闭 */
     closable?: boolean;
     type?: string;
-} 
+    style?: React.CSSProperties;
+    size?: string;
+}
 
 export default class Tag extends React.Component<ITagProps, {}> {
     public static defaultProps = {
@@ -16,41 +18,45 @@ export default class Tag extends React.Component<ITagProps, {}> {
     }
     constructor(props: ITagProps) {
         super(props);
-        this.state = {      
+        this.state = {
         }
     }
 
     public isPresetType(type?: string): boolean {
-        if (!type) {return false};
+        if (!type) { return false };
         return /(success|info|warning|danger)/gi.test(type)
     }
 
+    public isPresetSize(size?: string): boolean {
+        if (!size) { return false };
+        return /(medium|small|mini)/gi.test(size)
+    }
+
     public render() {
-        const { color, type } = this.props;
+        const { type, style, size, children, closable } = this.props;
         const isPresetType = this.isPresetType(type);
+        const isPresetSize = this.isPresetSize(size);
+        const closeIcon = closable ?
+            <i className="rc_el_tag_close rc_el_tag_icon_close"
+            // onClick={this.handleIconClick} 
+            />
+            : '';
         const classString = classNames('rc_el_tag', {
             [`rc_el_tag_${type}`]: isPresetType,
-            // [`${prefixCls}-has-color`]: (color && !isPresetColor),
+            [`rc_el_tag_${size}`]: isPresetSize,
             // [`${prefixCls}-close`]: this.state.closing,
             'test': true,
         });
         const tagStyle = {
-            backgroundColor: color
+            ...style
         }
         return (
-            <span className={classString} style={tagStyle}
-            // :class="[
-            //     type ? 'el-tag--' + type : '',
-            //     tagSize && `el-tag--${tagSize}`,
-            //     {'is-hit': hit}
-            // ]"
-            // :style="{backgroundColor: color}"
-            >
-            {/* <slot></slot> */}
-            {/* <i class="el-tag__close el-icon-close"
+            <span className={classString} style={tagStyle}>
+                {/* <i class="el-tag__close el-icon-close"
                 v-if="closable"
                 @click.stop="handleClose"></i> */}
-                tag
+                {children || 'tag'}
+                {closeIcon}
             </span>
         )
     }
